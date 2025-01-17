1bb9369f030c7f86299d939eef8b54ae3256436c
Rizki Firmansyah <rizfirman@gmail.com>
2025-01-17 22:56:14 +0700

    finish create responsive navbar

--- layouts/mainLayouts.vue
+++ layouts/mainLayouts.vue
@@ -1,7 +1,22 @@
 <template>
   <div>
-    <nav class="flex h-[83px] items-center justify-between border border-black">
-      <div class="flex">
+    <nav
+      class="flex w-full items-center justify-between border border-black lg:h-[83px]"
+    >
+      <!-- Burger Menu for Mobile/Tablet -->
+      <div
+        class="flex h-[56px] w-[56px] items-center justify-center border-r border-black lg:hidden"
+      >
+        <button
+          class="flex h-[50px] w-[50px] items-center justify-center"
+          @click="toggleMenu"
+        >
+          <Icon name="material-symbols:menu" size="24" />
+        </button>
+      </div>
+
+      <!-- Center Section (Desktop View) -->
+      <div class="hidden lg:flex">
         <nuxt-link to="/">
           <div
             class="flex h-[83px] w-[180px] items-center justify-center border-r border-black"
@@ -18,7 +33,9 @@
           </div>
         </nuxt-link>
       </div>
-      <div class="flex">
+
+      <!-- Right Section (Desktop View) -->
+      <div class="hidden lg:flex">
         <nuxt-link to="/">
           <div
             class="flex h-[83px] w-[180px] items-center justify-center border-l border-r border-black"
@@ -29,12 +46,50 @@
 
         <nuxt-link to="/">
           <div
-            class="flex h-[83px] w-[180px] items-center justify-center border-r border-black"
+            class="flex h-[83px] w-[180px] items-center justify-center border-black"
           >
             <p class="font-Gilroy text-base font-medium">Cart</p>
           </div>
         </nuxt-link>
       </div>
+
+      <!-- Cart Icon for Mobile/Tablet -->
+      <div
+        class="flex h-[56px] w-[56px] items-center justify-center border-l border-black lg:hidden"
+      >
+        <nuxt-link to="/cart">
+          <Icon name="material-symbols:shopping-cart" size="24" />
+        </nuxt-link>
+      </div>
     </nav>
+
+    <!-- Burger Menu Dropdown -->
+    <div
+      v-if="isMenuOpen"
+      class="absolute left-0 top-[60px] z-10 w-full border border-black bg-white lg:hidden"
+    >
+      <div class="flex flex-col items-center">
+        <nuxt-link to="/" class="w-full border-b border-black py-4 text-center">
+          Shop
+        </nuxt-link>
+        <nuxt-link to="/" class="w-full border-b border-black py-4 text-center">
+          Contact
+        </nuxt-link>
+        <nuxt-link to="/" class="w-full border-b border-black py-4 text-center">
+          Sign in
+        </nuxt-link>
+        <nuxt-link to="/" class="w-full border-b border-black py-4 text-center">
+          Cart
+        </nuxt-link>
+      </div>
+    </div>
   </div>
 </template>
+
+<script setup lang="ts">
+  const isMenuOpen = ref(false)
+
+  const toggleMenu = () => {
+    isMenuOpen.value = !isMenuOpen.value
+  }
+</script>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,6 +3,6 @@ export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
   css: ['@/assets/scss/global.scss'],
-  modules: ['@nuxtjs/tailwindcss', '@pinia/nuxt', '@nuxt/eslint'],
+  modules: ['@nuxtjs/tailwindcss', '@pinia/nuxt', '@nuxt/eslint', 'nuxt-icon'],
   ssr: false,
 })
