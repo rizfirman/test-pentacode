7f7f7a163e8debb353557a219dbf3e6244550f54
Rizki Firmansyah <rizfirman@gmail.com>
2025-01-19 22:09:27 +0700

    remove unused code

--- pages/index.vue
+++ pages/index.vue
@@ -5,11 +5,6 @@
     <AboutUsSection />
     <WhyChooseUsSection />
     <ContactUsSection />
-    <div class="col-span-12 border border-black">
-      <div class="flex h-[calc(100vh-83px)] items-center justify-center">
-        <p>Section Below</p>
-      </div>
-    </div>
   </div>
 </template>
 
